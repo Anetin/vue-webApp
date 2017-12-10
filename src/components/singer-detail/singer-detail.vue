f7c42b30563bc7aeb45a1a0291a8370af6a66897
Devins <1012886343@qq.com>
2017-12-10 15:53:27 +0800

    finish No-6 歌单详情

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,24 +1,72 @@
 <template id="">
   <transition name="slider">
-    <div class="singer-detail"></div>
+    <!-- <div class="singer-detail"></div> -->
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type="text/babel">
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
+  export default {
+    data () {
+      return {
+        songs: []
+      }
+    },
+    computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created () {
+      // console.log(this.singer)
+      this._getDetail()
+    },
+    methods: {
+      _getDetail () {
+        if (!this.singer.id) {
+          this.$router.push({ path: '../singer' })
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            // console.log(res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSongs (list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          // console.log(musicData)
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right:0
-    bottom: 0
-    background: $color-background
   .slider-enter-active, .slider-leave-active
     transition: all 0.3s
   .slider-enter,.slider-leave-to
